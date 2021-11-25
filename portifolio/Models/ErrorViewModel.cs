125b010798f9b62048337e4104d5bb7a4a151c97
Pedro Bunga <81700030+OrdepScripts@users.noreply.github.com>
2021-11-25 15:19:11 +0000

    Adicionar arquivos de projeto.

---
+++ portifolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace portifolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
